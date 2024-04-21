2a825f0adabfa827d3149d2e639b60cf35977241
Bhanuchander <162768959+Bhanuchanderragam@users.noreply.github.com>
2024-04-21 08:55:48 +0530

    Create notpage.js
    Signed-off-by: Bhanuchander <162768959+Bhanuchanderragam@users.noreply.github.com>

---
+++ React/notpage.js
@@ -0,0 +1,11 @@
+import React from ' react'
+
+const notpage=()=>{
+return{
+<div>
+ </h1 classname ="mt-5" style = {{fontsize ;" 40px",displsy:"flex ",justify content:" center", font weight;" 600"}}>page not found ;404 erro
+ </div>
+)
+}
+
+export default not page
