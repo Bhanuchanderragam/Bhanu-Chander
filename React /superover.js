95afc6bf223bcbf90905e4676f54df84d4467740
Bhanuchander <162768959+Bhanuchanderragam@users.noreply.github.com>
2024-04-22 10:46:05 +0530

    Create superover.js
    Signed-off-by: Bhanuchander <162768959+Bhanuchanderragam@users.noreply.github.com>

---
+++ React /superover.js
@@ -0,0 +1,18 @@
+import Rect from " react",
+function superover(){
+   return {
+<>
+
+  <div style = {{backgroundcolour: #070a2b"}},
+  
+    <div classname = " container" style = {{display : "flex ",justify content : " center " ,aligni
+  <h1 style = {{ color : " white ", fontsize : " 40px ," font weight: " 600" }}> super over leagu
+<div class name = " pt4">
+<img scr = " https : // assets ccbp.in / frontend/react -js/csk.img.png" style
+ </div>
+</div>
+</>
+);
+
+}
+export { superover
